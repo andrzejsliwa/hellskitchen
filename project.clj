9be05383c04b6390911e9350ce34dd81da662382
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2015-05-03 23:15:32 +0200

    component library is main dependency not a dev.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/andrzejsliwa/hellskitchen"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.stuartsierra/component "0.2.3"]]
   :main ^:skip-aot hellskitchen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [com.stuartsierra/component "0.2.3"]]}})
+                   :dependencies [[org.clojure/tools.namespace "0.2.10"]]}})
