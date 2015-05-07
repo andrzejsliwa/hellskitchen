e318a93d74e4cc20123d735fbce5ed28fecf70d9
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2015-05-07 21:14:14 +0200

    hexdump implementation.

--- src/hellskitchen/core.clj
+++ src/hellskitchen/core.clj
@@ -1,5 +1,6 @@
 (ns hellskitchen.core
-  (:require [com.stuartsierra.component :as component])
+  (:require [com.stuartsierra.component :as component]
+            [clojure.string :as string])
   (:gen-class))
 
 (defn system
@@ -10,3 +11,28 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn- count-hexdump-supply
+  [row-size data]
+  (let [size (count data)
+        reminder (rem size row-size)]
+    (if (= reminder 0)
+      0
+      (- row-size reminder))))
+
+(defn hexdump
+  "dump array of bytes to string containing hex values"
+  ([array]
+   (hexdump (count array) array))
+  ([row-size data]
+   (let [size (count  data)
+         reminder (rem size row-size)
+         supply-size (if (= reminder 0)
+                       0
+                       (- row-size reminder))]
+     (->> data
+          (concat (byte-array (count-hexdump-supply row-size data)))
+          (map #(format "%02x" %))
+          (partition row-size)
+          (map #(String/join " " %))
+          (string/join "\n")))))
--- test/hellskitchen/core_test.clj
+++ test/hellskitchen/core_test.clj
@@ -1,7 +1,15 @@
 (ns hellskitchen.core-test
-  (:require [clojure.test :refer :all]
-            [hellskitchen.core :refer :all]))
+  (:use midje.sweet)
+  (:require [hellskitchen.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "about `hexdump'"
+
+  (fact "it normally returns string representation of hex values"
+    (hexdump (byte-array 8)) => "00 00 00 00 00 00 00 00")
+
+  (fact "it splits array to rows with defined size"
+    (hexdump 4 (byte-array 8)) => "00 00 00 00\n00 00 00 00")
+
+  (fact "it fill missing bytes to match row size"
+    (hexdump 4 (byte-array 2)) => "00 00 00 00"
+    (hexdump 4 (byte-array 4)) => "00 00 00 00"))
